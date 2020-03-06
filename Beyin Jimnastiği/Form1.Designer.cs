46ad5419f34ef6619749b78fbdab3e68d8306521
bayansalduza <61632088+bayansalduza@users.noreply.github.com>
2020-03-06 22:40:36 +0300

    Add project files.

---
+++ Beyin Jimnastiği/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Beyin_Jimnastiği
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(73, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Beyin Jimnastiği";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label2.Location = new System.Drawing.Point(71, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Oyuna Başla";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.MouseLeave += new System.EventHandler(this.label2_MouseLeave);
+            this.label2.MouseHover += new System.EventHandler(this.label2_MouseHover);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(79, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(230, 182);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(41, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(268, 33);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Önceki Resimle Aynı Mı";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(12, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "72";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(199, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 47);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Hayır";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(75, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(64, 47);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "EVET";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 29);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 62);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Beyin_Jimnastiği.Properties.Resources.beyin_png;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(348, 377);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(700, 700);
+            this.MinimumSize = new System.Drawing.Size(322, 345);
+            this.Name = "Form1";
+            this.Text = "Beyin Jimnastiği";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Beyin Jimnastiği/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Beyin_Jimnastiği
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bm1;
+        Bitmap bm2;
+        int dogru = 0;
+        int yanlis = 0;
+        int sn = 72;
+        int index;
+        bool ctr1 = true;
+        List<Image> il = new List<Image>();
+        static bool karsi(Bitmap bm1, Bitmap bm2)
+        {
+            for (int i = 0; i < bm1.Width; i++)
+            {
+                for (int j = 0; j < bm1.Height; j++)
+                {
+                    Color cl1 = bm1.GetPixel(i, j);
+                    Color cl2 = bm2.GetPixel(i, j);
+                    if (cl1 != cl2)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_MouseHover(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.DarkBlue;
+        }
+
+        private void label2_MouseLeave(object sender, EventArgs e)
+        {
+            label2.ForeColor = Color.OrangeRed;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            this.BackgroundImage = default; 
+            pictureBox1.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+            label5.Visible = true;
+            timer1.Enabled = true;
+            Random r = new Random();
+            index = r.Next(il.Count());
+            pictureBox1.Image = il[index];
+            bm1 = new Bitmap(pictureBox1.Image);
+            ctr1 = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            il.Add(Properties.Resources.Adsız2_cutout);
+            il.Add(Properties.Resources.Adsız4_cutout);
+            il.Add(Properties.Resources.Adsız_cutout);
+            pictureBox1.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            label5.Visible = false;
+            Random r = new Random();
+            index = r.Next(il.Count());
+            timer1.Enabled = false;
+            pictureBox2.Visible = false;            
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sn--;
+            if(sn==70)
+            {
+                Random r = new Random();
+                index = r.Next(il.Count());
+                pictureBox1.Image = il[index];
+                if (ctr1 == false)
+                {
+                    bm2 = new Bitmap(pictureBox1.Image);
+                    ctr1 = true;
+                }
+                else
+                {
+                    bm1 = new Bitmap(pictureBox1.Image);
+                    ctr1 = false;
+                }
+            }
+            label6.Text = sn.ToString();
+            if (sn == 0)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Doğru= " + dogru.ToString() + ", Yanlış" + yanlis.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            index = r.Next(il.Count());
+            if (ctr1 == false)
+            {
+                bm2 = new Bitmap(pictureBox1.Image);
+                ctr1 = true;
+            }
+            else
+            {
+                bm1 = new Bitmap(pictureBox1.Image);
+                ctr1 = false;
+            }
+            if (!karsi(bm1, bm2))
+            {
+                dogru++;
+                pictureBox2.Image = Properties.Resources.pngresmi;
+                pictureBox2.Visible = true;
+                timer2.Enabled = true;
+            }
+            else
+            {
+                yanlis++;
+                pictureBox2.Image = Properties.Resources.pngresmi__1_;
+                pictureBox2.Visible = true;
+                timer2.Enabled = true;
+            }
+            pictureBox1.Image = il[index];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            index = r.Next(il.Count());
+            if (ctr1 == false)
+            {
+                bm2 = new Bitmap(pictureBox1.Image);
+                ctr1 = true;
+            }
+            else
+            {
+                bm1 = new Bitmap(pictureBox1.Image);
+                ctr1 = false;
+            }
+            if (karsi(bm1, bm2))
+            {
+                dogru++;
+                pictureBox2.Image = Properties.Resources.pngresmi;
+                pictureBox2.Visible = true;
+                timer2.Enabled = true;
+            }
+            else
+            {
+                yanlis++;
+                pictureBox2.Image = Properties.Resources.pngresmi__1_;
+                pictureBox2.Visible = true;
+                timer2.Enabled = true;
+            }
+            pictureBox1.Image = il[index];
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            timer2.Enabled = false;
+        }
+    }
+}
